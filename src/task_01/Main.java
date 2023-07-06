7ed4e583ccb1637f81f2cfac4abefc73686fda19
pavly <pavlykas0124@gmail.com>
2023-07-06 15:42:26 +0300

    Add ability to scan user input and catch NumberFormatException

--- src/task_01/Main.java
+++ src/task_01/Main.java
@@ -1,4 +1,25 @@
 package task_01;
 
+import java.util.Scanner;
+
 public class Main {
+
+    static int age;
+
+    public static void main(String[] args) {
+    }
+
+    private static int getInput() {
+
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter your full age: ");
+            age = Integer.parseInt(scanner.nextLine());
+            scanner.close();
+        } catch (NumberFormatException notInt) {
+            System.out.println("Exception: " + notInt);
+        }
+
+        return age;
+    }
 }
