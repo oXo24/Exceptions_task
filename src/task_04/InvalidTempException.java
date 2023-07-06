86c5609ea5625fb8a373164b6974661ac446c905
pavly <pavlykas0124@gmail.com>
2023-07-06 17:34:32 +0300

    Add custom exception

---
+++ src/task_04/InvalidTempException.java
@@ -0,0 +1,7 @@
+package task_04;
+
+public class InvalidTempException extends Exception {
+    public InvalidTempException(String str) {
+        super(str);
+    }
+}
--- src/task_04/Main.java
+++ src/task_04/Main.java
@@ -1,4 +1,9 @@
 package task_04;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class Main {
+
 }
