5cecce7daee140aebdd6509b872a2fb48a0b5bb2
pavly <pavlykas0124@gmail.com>
2023-07-06 17:03:30 +0300

    Add static representing funds on clients card and utils

--- src/task_03/Main.java
+++ src/task_03/Main.java
@@ -1,4 +1,10 @@
 package task_03;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class Main {
+    static double funds = 12953;
+
 }
